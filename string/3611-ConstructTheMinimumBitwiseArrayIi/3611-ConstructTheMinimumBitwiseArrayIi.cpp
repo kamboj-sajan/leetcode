4f7264ed0fcc940738c9f2c8c2c042fe75bf89fd
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:27:06 +0530

    Create 3611-ConstructTheMinimumBitwiseArrayIi

---
+++ string/3611-ConstructTheMinimumBitwiseArrayIi/3611-ConstructTheMinimumBitwiseArrayIi.cpp
@@ -0,0 +1,13 @@
+// Last updated: 19/08/2026, 15:27:05
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        
+        vector<int> ans;
+        for(int n : nums) {
+            if(n != 2) ans.push_back(n - ((n + 1) & (-n - 1)) / 2);
+            else ans.push_back(-1);
+        }   
+        return ans;
+    }
+};
