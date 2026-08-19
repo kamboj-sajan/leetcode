8cc267310ea9bc0be770bb17b83e1b0895262161
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 16:14:10 +0530

    Create 7-ReverseInteger

---
+++ string/7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/08/2026, 16:14:09
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            
+            // Check for overflow before actually multiplying
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
+            
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+};
