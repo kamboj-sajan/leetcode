32f6fdd0018d3f0a25cecdc14ad7e8222c693db0
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:21:43 +0530

    Create 4005-MaximumTotalSubarrayValueI

---
+++ string/4005-MaximumTotalSubarrayValueI/4005-MaximumTotalSubarrayValueI.cpp
@@ -0,0 +1,9 @@
+// Last updated: 19/08/2026, 15:21:42
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int a = *max_element(nums.begin(), nums.end());
+        int b = *min_element(nums.begin(), nums.end());
+        return 1LL * (a - b) * k; 
+    }
+};
