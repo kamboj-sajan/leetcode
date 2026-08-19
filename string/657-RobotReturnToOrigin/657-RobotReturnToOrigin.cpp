59d17deae8d5350e363a2e849e4437cc9362d6cc
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:58:45 +0530

    Create 657-RobotReturnToOrigin

---
+++ string/657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,15 @@
+// Last updated: 19/08/2026, 15:58:44
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0, y =0;
+        for(int i=0; i<moves.size(); ++i){
+            if(moves[i] == 'U')y++;
+            else if(moves[i] == 'D')y--;
+            else if(moves[i] == 'R')x++;
+            else x--;
+        }
+        if(x == 0 && y == 0)return true;
+        else return false;
+    }
+};
