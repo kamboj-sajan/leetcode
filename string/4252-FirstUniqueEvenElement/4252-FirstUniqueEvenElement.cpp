a9532ba9b2d9e221123c659cdcfd756bcf92ebcf
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:16:57 +0530

    Create 4252-FirstUniqueEvenElement

---
+++ string/4252-FirstUniqueEvenElement/4252-FirstUniqueEvenElement.cpp
@@ -0,0 +1,16 @@
+// Last updated: 19/08/2026, 15:16:56
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+       unordered_map<int,int> mpp;
+        for(int i=0; i<nums.size(); ++i){
+            mpp[nums[i]]++;
+        }
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i]%2 == 0 && mpp[nums[i]] == 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
