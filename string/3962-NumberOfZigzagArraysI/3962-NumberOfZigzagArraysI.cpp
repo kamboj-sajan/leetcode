e50722c7cd97e915dbee2c130fd2d79325a9cf54
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:22:59 +0530

    Create 3962-NumberOfZigzagArraysI

---
+++ string/3962-NumberOfZigzagArraysI/3962-NumberOfZigzagArraysI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/08/2026, 15:22:59
+class Solution {
+public:
+    static constexpr int MOD = 1000000007;
+    int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+        vector<int> dp(m, 1);
+
+        for (int i = 2; i <= n; i++) {
+            reverse(dp.begin(), dp.end());
+            int sum = 0;
+            for (auto& d : dp)
+                sum = (sum + exchange(d, sum)) % MOD;
+        }
+
+        return ((accumulate(dp.begin(), dp.end(), 0LL) % MOD) << 1) % MOD;
+    }
+};
