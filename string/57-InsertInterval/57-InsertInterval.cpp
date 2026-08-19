239025637b0e55f25d65758826556c7ef5bcefa8
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 16:11:15 +0530

    Create 57-InsertInterval

---
+++ string/57-InsertInterval/57-InsertInterval.cpp
@@ -0,0 +1,24 @@
+// Last updated: 19/08/2026, 16:11:14
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newinterval) {
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        int i = 0;
+        while(i < n && intervals[i][1] < newinterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        while(i<n && intervals[i][0] <= newinterval[1]){
+            newinterval[0] = min(newinterval[0],intervals[i][0]);
+            newinterval[1] = max(newinterval[1],intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newinterval);
+        while(i < n){
+            ans.push_back(intervals[i]);
+            ++i;
+        }
+        return ans;
+    }
+};
