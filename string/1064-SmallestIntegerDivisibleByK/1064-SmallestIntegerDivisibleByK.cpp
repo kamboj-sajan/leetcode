fee3f328060f9d5a4bbd5c272dc094dbaddaeb23
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:53:59 +0530

    Create 1064-SmallestIntegerDivisibleByK

---
+++ string/1064-SmallestIntegerDivisibleByK/1064-SmallestIntegerDivisibleByK.cpp
@@ -0,0 +1,14 @@
+// Last updated: 19/08/2026, 15:53:58
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k % 2 == 0 || k % 5 == 0) return -1;
+        int rem = 1 % k, len = 1;
+        while(rem > 0) {
+            rem = (rem * 10 + 1) % k;
+            len++;
+            if(len > k) return -1;
+        }
+        return len;   
+    }
+};
