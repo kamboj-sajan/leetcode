ca1c1e976dd6af4029def62789a3c85599b9d330
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 16:13:46 +0530

    Create 15-3sum

---
+++ string/15-3sum/15-3sum.cpp
@@ -0,0 +1,58 @@
+// Last updated: 19/08/2026, 16:13:45
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//       vector<vector<int>> ans;
+//       sort(nums.begin() , nums.end());
+//       for(int i=0; i<nums.size(); i++){
+//         if(i>0 && nums[i] == nums[i-1]) continue;
+//         int j = i + 1;
+//         int k = nums.size() - 1;
+//         while(j < k){
+//             int sum = nums[i] + nums[j] + nums[k];
+//             if(sum < 0 ) j++;
+//             else if(sum > 0) k--;
+//             else{
+//                 vector<int> temp = {nums[i], nums[j], nums[k]};
+//                 ans.push_back(temp);
+//                 j++;
+//                 k--;
+//                 while( j < k && nums[j] == nums[j-1]) j++;
+//                 while( j < k && nums[k] == nums[k+1]) k--;
+//             }
+//         }
+//       }
+//       return ans;  
+//     }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> sol;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; ++i)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k)
+            {
+                int total = nums[i] + nums[j] + nums[k];
+                if (total > 0)
+                    k--;
+                else if (total < 0)
+                    j++;
+                else
+                {
+                    sol.push_back({nums[i], nums[j++], nums[k]});
+                    while (nums[j] == nums[j - 1] && j < k)
+                        j++;
+                }
+            }
+        }
+        return sol;
+    }
+};
