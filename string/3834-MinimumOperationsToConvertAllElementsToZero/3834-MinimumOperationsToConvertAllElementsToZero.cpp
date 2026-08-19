20ddef9e0b3afd4d904c320ce0db0ee04b14daaf
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:25:01 +0530

    Create 3834-MinimumOperationsToConvertAllElementsToZero

---
+++ string/3834-MinimumOperationsToConvertAllElementsToZero/3834-MinimumOperationsToConvertAllElementsToZero.cpp
@@ -0,0 +1,17 @@
+// Last updated: 19/08/2026, 15:25:00
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        vector<int> stack(nums.size() + 1, 0);
+        int top = 0, ans = 0;
+        for (int num : nums) {
+            while (stack[top] > num) {
+                top--;
+                ans++;
+            }
+            if (stack[top] != num)
+                stack[++top] = num;
+        }
+        return ans + top;
+    }
+};
