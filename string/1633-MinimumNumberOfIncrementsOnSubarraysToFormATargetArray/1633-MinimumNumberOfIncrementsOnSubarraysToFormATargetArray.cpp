1efee8967a9971ef40b92ee3e03b4f2e3caeb71e
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:46:55 +0530

    Create 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray

---
+++ string/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 19/08/2026, 15:46:54
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int n=target.size(), ans=target[0];
+        for(int i=1; i<n; i++){
+            ans+=max(target[i]-target[i-1], 0);
+        }
+        return ans;
+    }
+};
