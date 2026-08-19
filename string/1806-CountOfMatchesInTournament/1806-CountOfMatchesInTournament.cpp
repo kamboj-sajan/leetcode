d77912afc3c0ec17bf3d94011e75b709b37164b1
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:37:41 +0530

    Create 1806-CountOfMatchesInTournament

---
+++ string/1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/08/2026, 15:37:41
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int cnt = 0;
+        while(n > 1){
+            if(n%2 ==0){
+                cnt += n/2;
+                n /= 2;
+            }else{
+                cnt += (n-1)/2;
+                n = (n-1)/2 + 1;
+            }
+        }
+        return cnt;
+        
+    }
+};
