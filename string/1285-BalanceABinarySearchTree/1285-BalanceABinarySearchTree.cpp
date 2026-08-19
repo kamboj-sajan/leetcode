2b6d8565a02b6e18ade2c7c81d3d5bcae30f89bc
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:51:35 +0530

    Create 1285-BalanceABinarySearchTree

---
+++ string/1285-BalanceABinarySearchTree/1285-BalanceABinarySearchTree.cpp
@@ -0,0 +1,38 @@
+// Last updated: 19/08/2026, 15:51:34
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& vals) {
+    if (!root) return;
+    inorder(root->left, vals);
+    vals.push_back(root->val);
+    inorder(root->right, vals);
+}
+    TreeNode* build(int l, int r, vector<int>& vals) {
+    if (l > r) return nullptr;
+    
+    int mid = (l + r) / 2;
+    TreeNode* root = new TreeNode(vals[mid]);
+    
+    root->left = build(l, mid - 1, vals);
+    root->right = build(mid + 1, r, vals);
+    
+    return root;
+}
+
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> vals;
+        inorder(root, vals);
+        return build(0, vals.size() - 1, vals);
+    }
+};
