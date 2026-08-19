289b5933e967a214e0d3b86f04e37741497c56db
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:26:43 +0530

    Create 3626-SmallestDivisibleDigitProductI

---
+++ string/3626-SmallestDivisibleDigitProductI/3626-SmallestDivisibleDigitProductI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 19/08/2026, 15:26:42
+class Solution {
+public:
+    bool check(int x, int t) {
+        int prod = 1;
+        while (x > 0) {
+            prod *= (x % 10);
+            x /= 10;
+        }
+        return prod % t == 0;
+    }
+
+    int smallestNumber(int n, int t) {
+        while (!check(n, t)) {
+            n++;
+        }
+        return n;
+    }
+};
