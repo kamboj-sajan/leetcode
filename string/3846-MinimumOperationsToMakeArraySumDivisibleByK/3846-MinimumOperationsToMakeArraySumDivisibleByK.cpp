d0031970531847e92a5a1b5d677d8c0e77f29eb3
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:24:55 +0530

    Create 3846-MinimumOperationsToMakeArraySumDivisibleByK

---
+++ string/3846-MinimumOperationsToMakeArraySumDivisibleByK/3846-MinimumOperationsToMakeArraySumDivisibleByK.cpp
@@ -0,0 +1,11 @@
+// Last updated: 19/08/2026, 15:24:54
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); ++i){
+            ans += nums[i];
+        }
+        return ans%k;
+    }
+};
