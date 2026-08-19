14afecd3e5950959adbf29457cb795b2adc5e101
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-08-19 15:22:04 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ string/3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/08/2026, 15:22:02
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // int cnt1 = 1;
+        // int cnt2 = 2;
+        // int sum1 = 0;
+        // int sum2 = 0;
+        // for(int i=0; i<n; ++i){
+        //     sum1 += cnt1;
+        //     sum2 += cnt2;
+        //     cnt1 += 2;
+        //     cnt2 += 2;
+        // }
+        // return gcd(sum1,sum2);
+        return n;
+    }
+};
