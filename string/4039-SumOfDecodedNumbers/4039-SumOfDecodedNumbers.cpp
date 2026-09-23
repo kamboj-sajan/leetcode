b57ed10ec55701ff344e97f4da62dc9066bfce57
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-24 00:48:36 +0530

    Create 4039-SumOfDecodedNumbers

---
+++ string/4039-SumOfDecodedNumbers/4039-SumOfDecodedNumbers.cpp
@@ -0,0 +1,34 @@
+// Last updated: 24/09/2026, 00:48:36
+class Solution {
+public:
+    using ll = long long;
+    int mod = 1e9 + 7;
+    ll ff(ll a, ll b){
+        ll ans = 1;
+        while(b > 0){
+            if(b %2 == 1){
+                ans = (ans*a)%mod;
+            }
+            a = (a*a)%mod;
+            b = b/2;
+        }
+        return ans;
+    }
+public:
+    int sumDecoded(vector<long long>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ll a = nums[i];
+            int w = (nums[i]%10);
+            ll d = floor(nums[i]/10);
+            string st = to_string(d);
+            string f = st.substr(0,w);
+            string l = st.substr(w);
+            ll f1 = stoll(f);
+            ll l1 = stoll(l);
+            ans = (ans + ff(f1,l1))%mod;
+        }
+        return ans;
+    }
+};
