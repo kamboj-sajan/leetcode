e309a1922dcc253f3b4798dac81ce779c819e1f6
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:00:00 +0530

    Create 3397-MaximumNumberOfDistinctElementsAfterOperations

---
+++ string/3397-MaximumNumberOfDistinctElementsAfterOperations/3397-MaximumNumberOfDistinctElementsAfterOperations.cpp
@@ -0,0 +1,16 @@
+// Last updated: 25/09/2026, 01:00:00
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, prev = -1e9;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int l = max(nums[i] - k, prev + 1);
+            if (l <= nums[i] + k) {
+                prev = l, ans++;
+            }
+        }
+        return ans;
+    }
+};
