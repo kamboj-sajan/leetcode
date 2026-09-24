7de93d895fe46db9c382565e614b569ba4b8dd4d
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 00:53:18 +0530

    Create 3741-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ string/3741-MinimumDistanceBetweenThreeEqualElementsIi/3741-MinimumDistanceBetweenThreeEqualElementsIi.cpp
@@ -0,0 +1,24 @@
+// Last updated: 25/09/2026, 00:53:17
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+         int n = nums.size();
+        unordered_map<int,vector<int>> mpp;
+        for(int i=0; i<n; ++i){
+            mpp[nums[i]].push_back(i);
+        }
+        int ans = INT_MAX;
+        for(auto &p : mpp){
+            auto &v = p.second;
+            if(v.size() < 3)continue;
+            for(int i=0; i<v.size()-2; ++i){
+                int d = 2 * (v[i+2] - v[i]);
+                ans = min(ans,d);
+            }
+        }
+        if(ans == INT_MAX){
+            ans = -1;
+        }
+        return ans;
+    }
+};
