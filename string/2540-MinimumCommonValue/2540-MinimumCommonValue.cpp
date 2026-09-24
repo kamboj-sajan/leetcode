cfb4aaf0224647a4d9d015a18bb3ef4cedb1d62d
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:04:06 +0530

    Create 2540-MinimumCommonValue

---
+++ string/2540-MinimumCommonValue/2540-MinimumCommonValue.cpp
@@ -0,0 +1,18 @@
+// Last updated: 25/09/2026, 01:04:05
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i]; // smallest common
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1; // no common element
+    }
+};
