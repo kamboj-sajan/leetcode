8b9f7b120e6e579895ed91799fe812dc401f5e69
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:09:44 +0530

    Create 1757-RecyclableAndLowFatProducts

---
+++ string/1757-RecyclableAndLowFatProducts/1757-RecyclableAndLowFatProducts.sql
@@ -0,0 +1,8 @@
+-- Last updated: 25/09/2026, 01:09:43
+-- # Write your MySQL query statement below
+-- Select product_id from products where low_fats = 'Y' and recyclable = 'Y';
+
+
+SELECT product_id 
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
