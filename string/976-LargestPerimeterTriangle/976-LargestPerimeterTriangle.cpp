d2414f4b4e5bccd79830a407f42d3f246097522f
Sajan Kamboj <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:18:11 +0530

    Create 976-LargestPerimeterTriangle

---
+++ string/976-LargestPerimeterTriangle/976-LargestPerimeterTriangle.cpp
@@ -0,0 +1,11 @@
+// Last updated: 25/09/2026, 01:18:11
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+         sort(nums.begin(), nums.end(), greater<int>());
+        for (int i = 0; i < nums.size() - 2; i++)
+            if (nums[i + 1] + nums[i + 2] > nums[i])
+                return nums[i] + nums[i + 1] + nums[i + 2];
+        return 0;
+    }
+};
