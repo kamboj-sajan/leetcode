a6b74fae8f306c95a4b636109ba6a0c77cc367bb
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:13:08 +0530

    Create 1411-NumberOfWaysToPaintN3Grid

---
+++ string/1411-NumberOfWaysToPaintN3Grid/1411-NumberOfWaysToPaintN3Grid.cpp
@@ -0,0 +1,19 @@
+// Last updated: 25/09/2026, 01:13:07
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD = 1e9 + 7;
+        
+        long long a = 6; // Type A (ABA)
+        long long b = 6; // Type B (ABC)
+
+        for (int i = 2; i <= n; i++) {
+            long long na = (a * 3 + b * 2) % MOD;
+            long long nb = (a * 2 + b * 2) % MOD;
+            a = na;
+            b = nb;
+        }
+
+        return (a + b) % MOD;
+    }
+};
