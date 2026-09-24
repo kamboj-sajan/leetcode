8adfbd02c69d068cbd7a81a625b843f4cfae10ab
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:01:19 +0530

    Create 3121-CountTheNumberOfSpecialCharactersIi

---
+++ string/3121-CountTheNumberOfSpecialCharactersIi/3121-CountTheNumberOfSpecialCharactersIi.cpp
@@ -0,0 +1,29 @@
+// Last updated: 25/09/2026, 01:01:19
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> lastLower(26, -1), firstUpper(26, 1e9);
+        
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+            
+            if (islower(ch)) {
+                lastLower[ch - 'a'] = i;
+            } else {
+                firstUpper[ch - 'A'] = min(firstUpper[ch - 'A'], i);
+            }
+        }
+        
+        int ans = 0;
+        
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 &&
+                firstUpper[i] != 1e9 &&
+                lastLower[i] < firstUpper[i]) {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
