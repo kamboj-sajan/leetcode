4798ec0d73ffce0452657ee208f6934974c28444
kamboj-sajan <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:16:20 +0530

    Create 1108-DefangingAnIpAddress

---
+++ string/1108-DefangingAnIpAddress/1108-DefangingAnIpAddress.cpp
@@ -0,0 +1,15 @@
+// Last updated: 25/09/2026, 01:16:19
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(int i=0; i<address.size(); ++i){
+            if(address[i] == '.'){
+                ans += "[.]";
+            }else{
+                ans += address[i];
+            }
+        }
+        return ans;
+    }
+};
