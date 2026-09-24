e2f1f24b8a04e7b9e0ff596d1563491ebb3df6d2
Sajan Kamboj <147792758+kamboj-sajan@users.noreply.github.com>
2026-09-25 01:20:25 +0530

    Create 700-SearchInABinarySearchTree

---
+++ string/700-SearchInABinarySearchTree/700-SearchInABinarySearchTree.cpp
@@ -0,0 +1,31 @@
+// Last updated: 25/09/2026, 01:20:25
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     TreeNode* searchBST(TreeNode* root, int val) {
+//         while(root != NULL && root->val != val){
+//             root = val<root->val ? root->left : root->right;
+//         }
+//         return root;
+//     }
+// };
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL || root->val==val){
+            return root;
+        }
+        return val<root->val ? searchBST(root->left,val) : searchBST(root->right,val);
+    }
+};
